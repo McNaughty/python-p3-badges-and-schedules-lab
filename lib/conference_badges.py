074dc189b2897dbf33d171e943a29be1fa1c6946
Winnie <wimacoj@gmail.com>
2024-01-20 17:59:58 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,33 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = f'Hello, my name is {name}.'
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    room = 1
+    assigned_rooms = []
+    for name in names:
+        allocation = f'Hello, {name}! You\'ll be assigned to room {room}!'
+        assigned_rooms.append(allocation)
+        room += 1
+    return assigned_rooms
+# print(assign_rooms)       
+    
 
 def printer(names):
-    return None
+    # Generate badges using batch_badge_creator
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+
+    # Assign rooms using assign_rooms
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
+
+
